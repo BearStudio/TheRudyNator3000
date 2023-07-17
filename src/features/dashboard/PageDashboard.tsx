76491ad28636789679ef24455090dd0d824e37f7
Renan <renan.decamps@gmail.com>
2023-07-17 11:57:12 +0200

    fix: clean mocked data and unused imports

--- src/features/dashboard/PageDashboard.tsx
+++ src/features/dashboard/PageDashboard.tsx
@@ -1,22 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 import {
   Box,
   Button,
   SimpleGrid,
-  Stack,
-  Text,
   Textarea,
   useClipboard,
 } from '@chakra-ui/react';
 import { Formiz, useForm } from '@formiz/core';
 import { motion } from 'framer-motion';
-import { FiInfo } from 'react-icons/fi';
 import useSound from 'use-sound';
 
 import { FieldSelect } from '@/components/FieldSelect';
 import { FieldTextarea } from '@/components/FieldTextarea';
-import { Icon } from '@/components/Icons';
 import { Page, PageContent } from '@/components/Page';
 import { AdvancedSettings } from '@/features/dashboard/AdvancedSettings';
 import { Header } from '@/features/dashboard/Header';
@@ -32,7 +28,6 @@ export default function PageDashboard() {
 
   const getTextMutation = useAskAI({
     onSuccess(res) {
-      // TODO: ajout toast texte copié avec succès
       clipboard.setValue(res.response);
     },
   });
@@ -58,17 +53,7 @@ export default function PageDashboard() {
         });
     },
     initialValues: {
-      emailContext: `Bonjour Monsieur Baer,
-Le CIR 2022 est terminé mais nous nous mobilisons dès aujourd'hui avec nos clients pour le CIR 2023.
-Bearstudio est peut-être confrontée en ce moment même à un contrôle du CIR. Si le sujet n'a pas été piloté de manière continue avec un référent interne ou un partenaire leader sur le sujet, vos équipes et vous-mêmes doivent se mobiliser dans l'urgence pour limiter le risque.
-Notre expérience depuis 2006 nous confirme que la sécurisation et l'optimisation du CIR est le fruit d'une démarche continue et non pas l'affaire de quelques jours dans l'année.
-Je souhaitais vous partager en quelques slides notre approche qui fait notre succès auprès des entreprises de croissance (Mirakl, Qonto, Opinel, Nanobiotix,...) :
-Les types de sollicitations fiscales
-Les indispensables du dossier CIR à préparer
-Les étapes clés d'une mission CIR performante et anticipée
-Voir le document : préparer son CIR 2023
-Bonne lecture,
-Bonne journée,`,
+      emailContext: '',
       subjectContext: subjects[0]?.label,
       subjectRules: subjects[0]?.description,
       settings: {
